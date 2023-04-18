12a1372234f6e49b018552b4c2b19d6a97a70c3e
Jack Parmer <jparmer09@gmail.com>
2023-04-18 00:49:04 -0400

    site speed up

--- litebox.js
+++ litebox.js
@@ -3,13 +3,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
     const litebox = document.querySelector('.litebox');
 
-    let liteboxSize = 'transparent 160px, rgba(0, 0, 0, 0.85) 200px)';
+    let liteboxSize = 'transparent 100px, rgba(0, 0, 0, 0.85) 100px)';
 
     window.addEventListener('mousemove', e => updateLitebox(e));
 
+    window.addEventListener('scroll', e => updateLitebox(e));
+
     window.addEventListener('mousedown', e => {
 
-        liteboxSize = 'transparent 130px, rgba(0, 0, 0, 0.95) 150px)';
+        liteboxSize = 'transparent 50px, rgba(0, 0, 0, 0.95) 50px)';
 
         updateLitebox(e);
 
@@ -17,15 +19,47 @@ window.addEventListener("DOMContentLoaded", () => {
 
     window.addEventListener('mouseup', e => {
 
-        liteboxSize = 'transparent 160px, rgba(0, 0, 0, 0.85) 200px)';
+        liteboxSize = 'transparent 100px, rgba(0, 0, 0, 0.85) 100px)';
 
         updateLitebox(e);
 
     });
 
+    window.addEventListener('mouseup', e => {
+
+        liteboxSize = 'transparent 100px, rgba(0, 0, 0, 0.85) 100px)';
+
+        updateLitebox(e);
+
+    });    
+
     function updateLitebox(e) {
 
-        litebox.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 100}%, ${liteboxSize}`;
+        litebox.style.backgroundImage = `radial-gradient(circle at ${e.pageX / window.innerWidth * 100}% ${e.pageY / window.innerHeight * 50}%, ${liteboxSize}`;
 
     }
 });
+
+document.addEventListener(
+    "click",
+    function (event) {
+      // Only fire if the target has id copy
+      if (!event.target.matches("#copy")) return;
+  
+      if (!navigator.clipboard) {
+        // Clipboard API not available
+        return;
+      }
+      const text = event.target.value;
+      try {
+        navigator.clipboard.writeText(text);
+        document.getElementById("copy-status").innerText = "🧞‍♂️ copied to clipboard";
+        setTimeout(function () {
+          document.getElementById("copy-status").innerText = "👈 click to copy";
+        }, 1200);
+      } catch (err) {
+        console.error("Failed to copy!", err);
+      }
+    },
+    false
+  );
